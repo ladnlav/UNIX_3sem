40d0161f39a182c18ca72375f13aa590ec504e2d
Nesterenko Vladislav <101095507+ladnlav@users.noreply.github.com>
2022-03-23 22:43:46 +0300

    Update dz1_echo.sh

--- dz1_echo.sh
+++ dz1_echo.sh
@@ -1,4 +1,4 @@
-source /C/Users/Владислав/.basheditor/remote-debugging-v2.sh localhost 33333 #BASHEDITOR-TMP-REMOTE-DEBUGGING-END|Origin line:
+
 echo "Hello, world!!!"
 echo "I'm $USER"
 echo "Today:" `date`
